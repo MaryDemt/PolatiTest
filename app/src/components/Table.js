3a1389ae6bde473863f7ef9f236d8a17526609a5
MaryDemt <mary.shevtcova@yandex.ru>
2022-06-20 19:21:40 +0300

    add sorting

--- app/src/components/Table.js
+++ app/src/components/Table.js
@@ -1,32 +1,47 @@
-import Table from 'react-bootstrap/Table';
 
-function Table1(props) {
 
-  console.log(props.data)
-    return (
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px'}}>
-        <table>
-        <thead>
-    <tr>
-      <th width="20%">Name</th>
-      <th>Surname</th>
-      <th>Patronymic</th>
-      <th>Birthday</th>
-    </tr>
-        </thead>
-  <tbody>
-  { props.data.map(item =>(
-                <tr key={item.name}>
-                    <td>{item.name}</td>
-                    <td>{item.surname}</td>
-                    <td>{item.middleName}</td>
-                    <td>{item.birthday}</td>
-                </tr>
-            ))}
-  </tbody>
-      </table>
-      </div>
-    );
-  }
- 
+const Table1 = ({sortData, contactData}) => {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px'}}>
+    <table
+      border="1px"
+      style={{
+        borderCollapse: "collapse",
+        borderSpacing: "10px 30px",
+        width: "400px",
+      }}
+    >
+      <thead>
+        <tr>
+          <th></th>
+          <th onClick={() => sortData("name")}>
+              Name
+          </th>
+          <th onClick={() => sortData("surname")}>
+              Surname
+          </th>
+          <th onClick={() => sortData("middleName")}>
+              Patronymic
+          </th>
+          <th onClick={() => sortData("birthday")}>
+              Birthday
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {contactData.map((item) => (
+          <tr key={item.name}>
+            <td>{contactData.indexOf(item)+1}</td>
+            <td>{item.name}</td>
+            <td>{item.surname}</td>
+            <td>{item.middleName}</td>
+            <td>{item.birthday}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
+};
+
 export default Table1;
